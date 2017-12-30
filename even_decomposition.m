8bbb05d561d932c4b164fb63c09efa1ac588a7a0
祝超 <18292885866@163.com>
2017-12-31 00:02:02 +0800

    update even_decomposition.m
    新增了第二种时间代价更大的方法；使用了模块化方法，方便调试和检验

--- even_decomposition.m
+++ even_decomposition.m
@@ -32,3 +32,38 @@ for part1 = 3:even/2 %如果part1=2，则part2仍为偶数，所以从3开始
     end
 end
 end
+
+%% 第二种分解
+function even_decomposition2(num)
+% 分解为两个质数part1与part2之和
+for part1 = 1:num/2
+    part2 = num - part1;
+    if check(part1)==1 && check(part2)==1 %part1与part2均为质数，则打印输出
+        disp([num2str(num),'=',num2str(part1),'+',num2str(part2)]);
+    end
+end
+end
+% 检验是否为质数
+function flag = check(num)
+flag = 1;
+if num~=1
+    for i = 2:num-1
+        if mod(num,i)==0
+        flag = 0;
+        end
+    end
+end
+end
+% 更快速的检验方法
+function flag = check2(num)
+k = floor(sqrt(num));
+i = 2;
+while (mod(num, i)~=0)&&(i<=k)
+    i = i + 1;
+end
+if i>k
+    flag = 1;
+else
+    flag = 0;
+end
+end
